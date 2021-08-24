a5241f07c4d7df62ae65e694114c3575ac255699
Gabbyyy420 <89363852+Gabbyyy420@users.noreply.github.com>
2021-08-24 20:43:19 +0800

    Create logic.v

---
+++ logic.v
@@ -0,0 +1,17 @@
+# Data-Types
+bit                             – Unsigned
+byte, shortint, int, longint    – Signed
+   unsigned two-state types,
+      bit            single_bit ;    // unsigned single bit
+      bit [31:0]     32_bit      ;    // 32-bit unsigned integer
+   signed two-state types,
+      int           integer;   // 32-bit signed integer
+      byte          8_bit  ;   //   8-bit signed integer
+      shortint      16_bit ;   // 16-bit signed integer
+      longint       64_bit ;   // 64-bit signed integer
+   unsigned from signed two-state types,
+       int           unsigned integer;   // 32-bit unsigned integer
+       byte          unsigned  8_bit;   //   8-bit unsigned integer
+       shortint      unsigned  16_bit;   // 16-bit unsigned integer
+       longint       unsigned  64_bit;   // 64-bit unsigned integer
+© 2021 GitHub, Inc.
